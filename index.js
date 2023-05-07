875b8b48c2f7dba81b15ded91dd002327216c047
bilalhossainjoy <billalhossain.bhj@gmail.com>
2023-05-07 15:40:52 +0600

    next

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const hbs = require('hbs')
+
+app.use(express.static('public'))
+
+app.set('view engine','hbs');
+
+hbs.registerPartials(path.join(__dirname,'./views/partials'))
+
+app.config = {
+    port: process.env.port | 8080,
+    host: '192.168.1.1'
+}
+
+app.get('/',(req,res)=>{
+    res.render('./layouts/home')
+})
+
+app.get('/about',(req,res)=>{
+    res.render('./layouts/about')
+})
+
+app.get('/projects',(req,res)=>{
+    res.render('./layouts/projects')
+})
+
+app.get('/',(req,res)=>{
+    res.render('./layouts/info')
+})
+
+app.get('*',(req,res)=>{
+    res.render('./layouts/404NotFound')
+})
+
+app.listen(app.config.port, app.config.host,()=>{
+    console.log(`server running on http://${app.config.host}:${app.config.port}`)
+})
 
 
